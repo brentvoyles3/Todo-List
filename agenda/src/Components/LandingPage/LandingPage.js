b81a2fe6c546c7791aabdfd3d7c6c1ea7f3922e1
Brent+ Voyles <brent.voyles3@uga.edu>
2023-09-01 13:28:56 -0400

    Changed to a functional approach

--- agenda/src/Components/LandingPage/LandingPage.js
+++ agenda/src/Components/LandingPage/LandingPage.js
@@ -3,80 +3,95 @@ import 'bootstrap/dist/css/bootstrap.css';
 import './main.css';
 import Popup from '../Popup/Popup';
 import axios from 'axios';
+import { v4 as uuidv4 } from 'uuid';
 import { Link } from "react-router-dom";
 import { Modal, Button } from "react-bootstrap";
 
-class LandingPage extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            count: 0,
-            isOpen: false
-    };
-    }
-    openModal = () => this.setState({ isOpen: true });
-    closeModal = () => this.setState({ isOpen: false });
 
-    saySomething(something) {
-        console.log(something);
-    }
+const initialList = [
+  {
+    id: 'a',
+    chore: 'Clean Clothes',
+  },
+  {
+    id: 'b',
+    chore: 'Workout',
+  },
+];
 
-    handleClick(e) {
-        this.saySomething("element clicked");
-    }
 
-    componentDidMount() {
-        this.saySomething("component did mount");
-    }
+const listReducer = (state, action) => {
+  switch (action.type) {
+    case 'ADD_ITEM':
+      return {
+        ...state,
+        list: state.list.concat({ chore: action.chore, id: action.id }),
+      };
+    default:
+      throw new Error();
+      console.log("Not good");
+  }
+};
 
-    render() {
-        return (
-            <div className="total">
-                {/* Website landing page content */}
-                <div className="body">
-                        {/* Add content */}
-                        <h1>
-                            Todo List
-                        </h1>
-                <div className="items">
-                <Button variant="primary" onClick={this.openModal}> Add item </Button>
-                <Modal show={this.state.isOpen}>{/* ... */}</Modal>
-                <Modal show={this.state.isOpen} onHide={this.closeModal}>
-          <Modal.Header closeButton>
-            <Modal.Title>Add Item</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>Which item would you like to add to the list?</Modal.Body>
-          <Modal.Footer>
-            <Button variant="secondary" onClick={this.closeModal}>
-              Close
-            </Button>
-          </Modal.Footer>
-        </Modal>
-                <select>
-                    <option value="chore1">Exercise</option>
-                    <option value="chore2">Wash Car</option>
-                    <option value="chore3">Workout</option>
-                    <option value="chore4">Clean House</option>
-                    <option value="chore5">Laundry</option>
-                </select>
-                <Button variant="primary" onClick={this.openModal}> Complete Item </Button>
-                <Modal show={this.state.isOpen}>{/* ... */}</Modal>
-                <Modal show={this.state.isOpen} onHide={this.closeModal}>
-          <Modal.Header closeButton>
-            <Modal.Title>Remove Item</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>Which item would you like to remove from the list?</Modal.Body>
-          <Modal.Footer>
-            <Button variant="secondary" onClick={this.closeModal}>
-              Close
-            </Button>
-          </Modal.Footer>
-        </Modal>
-                </div>
-      </div>
-            </div>
-        );
-        }
-}
+const App = () => {
+  const [listData, dispatchListData] = React.useReducer(listReducer, {
+    list: initialList,
+    isShowList: true,
+  });
+  const [chore, setchore] = React.useState('');
 
-export default LandingPage
+  function handleChange(event) {
+    setchore(event.target.value);
+  }
+
+  function handleAdd() {
+    dispatchListData({ type: 'ADD_ITEM', chore, id: uuidv4() });
+    setchore('');
+  }
+
+  return (
+    <div className="body">
+    {/* Add content */}
+    <h1>
+        Todo List
+    </h1>
+    <div>
+      <AddItem
+        chore={chore}
+        onChange={handleChange}
+        onAdd={handleAdd}
+      />
+      <List list={listData.list} />
+    </div>
+    </div>
+  );
+};
+
+const AddItem = ({ chore, onChange, onAdd }) => (
+  <div className="add">
+        <button className="addButton" type="button" onClick={onAdd}>
+      Add Chore
+    </button>
+    <input type="text" value={chore} onChange={onChange} />
+  </div>
+);
+
+const RemoveItem = ({ chore, onChange, onAdd }) => (
+  <div className="remove">
+        <button className="removeButton" type="button" onClick={onAdd}>
+      Remove Chore
+    </button>
+  </div>
+);
+
+const List = ({ list }) => (
+<div className="addList">
+  <ul>
+    {list.map((item) => (
+      <li key={item.id}>{item.chore}</li>
+    ))}
+  </ul>
+  </div>
+);
+
+export default App;
